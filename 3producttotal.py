9343989c127628d0f7521f3e83ef6272e91d5068
VirunaC <161706060+VirunaC@users.noreply.github.com>
2024-03-05 15:42:23 +0530

    Add files via upload

---
+++ 3producttotal.py
@@ -0,0 +1,8 @@
+
+item1 = int(input('Enter price of product 1: '))
+item2 = int(input('Enter price of product 2: '))               
+item3 = int(input('Enter price of product 3: '))
+            
+total = ( item1 + item2 + item3 )
+            
+print("Total cost is ", total, "$")
