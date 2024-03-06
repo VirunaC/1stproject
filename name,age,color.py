c2bacd6455e18a1b672b5b83cbb2c1a2100c19c2
Viruna Cooray <161706060+VirunaC@users.noreply.github.com>
2024-03-06 11:59:22 +0530

    Add files via upload

---
+++ name,age,color.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input("Enter your favorite color: ")
+
+print("Hi ", name, ". You're ", age, " years old and your favorite color is ", color, ".")
