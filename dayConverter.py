0ac98c1c4096f3e138c36870564db202aa2b5c4a
Viruna Cooray <161706060+VirunaC@users.noreply.github.com>
2024-03-06 15:12:58 +0530

    Add files via upload

---
+++ dayConverter.py
@@ -0,0 +1,9 @@
+
+days = float(input("Enter number of days: "))
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print('No. of hours = ', hours)
+print('No. of minutes = ', minutes)
+print('No. of seconds = ', seconds)
